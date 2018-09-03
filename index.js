4b4a36bb4ea874e7f2e6078b009be01e4de6aa54
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2018-09-03 21:55:44 +0200

    Script now clones/pulls and checks out the given ref.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const git = require('simple-git')();
 
 const args = process.argv.slice(2);
 
@@ -12,5 +13,51 @@ function ensureDependencies() {
   const elmFile = fs.readFileSync('elm.json', { encoding: 'utf-8' });
   const elmJson = JSON.parse(elmFile);
   const gitDeps = elmJson['git-dependencies'];
-  console.log(gitDeps);
+
+  if (!fs.existsSync('elm-stuff/gitdeps')) {
+    fs.mkdirSync('elm-stuff/gitdeps');
+  }
+
+  for (const url in gitDeps) {
+    const ref = gitDeps[url];
+    const subPath = pathify(url);
+    const path = `elm-stuff/gitdeps/${subPath}`;
+
+    if (fs.existsSync(path)) {
+      updateDependency(path, ref);
+    } else {
+      cloneDependency(url, path, ref);
+    }
+  }
+}
+
+function pathify(url) {
+  const colon = url.indexOf(':') + 1;
+  let end = url.indexOf('.git');
+
+  if (end === -1) {
+    end = url.length;
+  }
+  
+  return url.slice(colon, end);
+}
+
+function cloneDependency(url, path, ref) {
+  console.log(`cloning ${url} into ${path} and checking out ${ref}`);
+
+  git.clone(url, path, () => {
+    const git = require('simple-git')(path);
+    git.checkout(ref);
+    console.log('done');
+  });
+}
+
+function updateDependency(path, ref) {
+  console.log(`updating ${path} to ${ref}`);
+  const git = require('simple-git')(path);
+
+  git.pull(() => {
+    git.checkout(ref);
+    console.log('done');
+  });
 }
