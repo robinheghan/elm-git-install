d0f27be4ea31294f43fc56d7b31b8d2e78b0e027
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2018-09-09 17:42:36 +0200

    Fixes #10: Recursive validation of elm.json.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const gitInPath = require('simple-git');
+const isGitUrl = require('is-git-url');
 
 const gitRoot = gitInPath(); // git client for current working directory
 const storagePath = path.join('elm-stuff', 'gitdeps');
@@ -17,7 +18,17 @@ if (args.length === 0) {
 
 function ensureDependencies() {
   const elmJson = readElmJson('');
-  const gitDeps = elmJson['git-dependencies'];
+
+  const verificationError = verifyElmJson(elmJson);
+  if (verificationError !== '') {
+    console.log(storagePath + ': Not valid elm.json file');
+    console.log(verificationError);
+    return;
+  }
+
+  const gitDeps = elmJson.type === 'application' ?
+        elmJson['git-dependencies'].direct
+        : elmJson['git-dependencies'];
 
   if (!fs.existsSync(storagePath)) {
     fs.mkdirSync(storagePath);
@@ -122,7 +133,15 @@ function refIsBranch(branchSummary, tagSummary, ref) {
 
 function afterCheckout(repoPath, opts, next) {
   const depElmJson = readElmJson(repoPath);
-  const depSources = depElmJson['source-directories'];
+
+  const verificationError = verifyPackageElmJson(depElmJson);
+  if (verificationError !== '') {
+    console.log(repoPath + ': Not valid elm json');
+    console.log(verificationError);
+    return;
+  }
+  
+  const depSources = ['src'];
   const depGitDeps = depElmJson['git-dependencies'] || {};
 
   next = ((next) => {
@@ -168,6 +187,8 @@ function dedupe(arr1, arr2) {
 }
 
 
+/* IO */
+
 function readElmJson(repoPath) {
   let elmFilePath = path.join(repoPath, 'elm.json');
   let gitDepsPath = path.join(repoPath, 'elm-git.json');
@@ -198,4 +219,208 @@ function writeElmJson(elmJson) {
 
   const newElmFile = JSON.stringify(newElmJson, null, 4);
   fs.writeFileSync('elm.json', newElmFile, { encoding: 'utf-8' });
+}
+
+
+/* VERIFICATION */
+
+function verifyElmJson(elmJson) {
+  const applicationError = verifyApplicationElmJson(elmJson);
+  const packageError = verifyPackageElmJson(elmJson);
+
+  if (applicationError === '') {
+    return '';
+  } else if (packageError === '') {
+    return '';
+  }
+
+  return applicationError;
+}
+
+function verifyApplicationElmJson(elmJson) {
+  if (elmJson.type !== "application") {
+    return 'Type field of elm.json has to be \'application\'';
+  }
+
+  const deps = elmJson['dependencies'];
+  if (!isObject(deps)) {
+    return "'dependencies' field in elm.json has to be an object";
+  }
+
+  const depsErr = checkAppDependencies(deps);
+  if (depsErr !== '') {
+    return depsErr
+  }
+
+  const gitDeps = elmJson['git-dependencies'];
+  if (!isObject(gitDeps)) {
+    return "'git-dependencies' field in elm-git.json has to be an object";
+  }
+
+  const gitDepsErr = checkAppGitDependencies(gitDeps);
+  if (gitDepsErr !== '') {
+    return gitDepsErr
+  }
+  
+  return '';
+}
+
+function checkAppDependencies(deps) {
+  const directDeps = deps['direct'];
+  const directDepsErr = `'$dependencies.direct' in elm.json should be an object of name => semver.`;
+  const directDepsReturn = checkAppDependenciesObject(directDeps, directDepsErr);
+  if (directDepsReturn !== '') {
+    return directDepsReturn;
+  }
+
+
+  const indirectDeps = deps['indirect'];
+  const indirectDepsErr = `'$dependencies.direct' in elm.json should be an object of name => semver.`;
+  const indirectDepsReturn = checkAppDependenciesObject(indirectDeps, indirectDepsErr);
+  if (indirectDepsReturn !== '') {
+    return indirectDepsReturn;
+  }
+
+  return '';
+}
+
+function checkAppDependenciesObject(deps, depsErr) {
+  if (!isObject(deps)) {
+    return depsErr;
+  }
+
+  for (const key in deps) {
+    const val = deps[key];
+
+    if (!isProjectName(key)) {
+      return depsErr;
+    }
+
+    if (toSemVer(val) == null) {
+      return depsErr;
+    }
+  }
+
+  return '';
+}
+
+function checkAppGitDependencies(deps) {
+  const directDeps = deps['direct'];
+  const directDepsErr = "'$git-dependencies.direct' in elm-git.json should be an object of name => semver.";
+  const directDepsReturn = checkAppGitDependenciesObject(directDeps, directDepsErr);
+  if (directDepsReturn !== '') {
+    return directDepsReturn;
+  }
+
+
+  const indirectDeps = deps['indirect'];
+  const indirectDepsErr = "'$git-dependencies.indirect' in elm-git.json should be an object of name => semver.";
+  const indirectDepsReturn = checkAppGitDependenciesObject(indirectDeps, indirectDepsErr);
+  if (indirectDepsReturn !== '') {
+    return indirectDepsReturn;
+  }
+
+  return '';
+}
+
+function checkAppGitDependenciesObject(deps, depsErr) {
+  if (!isObject(deps)) {
+    return depsErr;
+  }
+
+  for (const key in deps) {
+    const val = deps[key];
+
+    if (!isGitUrl(key)) {
+      return depsErr;
+    }
+  }
+
+  return '';
+}
+
+function verifyPackageElmJson(elmJson) {
+  if (elmJson.type !== 'package') {
+    return "'type' field in elm.json has to be 'package'";
+  }
+
+  const deps = elmJson['dependencies'];
+  if (!isObject(deps)) {
+    return "'dependencies' field in elm.json should be an object of name => semver range.";
+  }
+
+  for (const key in deps) {
+    const val = deps[key];
+
+    if (toSemVerRange(val) == null) {
+      return "'dependencies' field in elm.json should be an object of name => semver range.";
+    }
+  }
+
+  const gitDepErr = "'git-dependencies' field in elm-git.json should be an object of git-url => semver range.";
+
+  const gitDeps = elmJson['git-dependencies'];
+  if (!isObject(deps)) {
+    return gitDepErr;
+  }
+
+  for (const key in gitDeps) {
+    if (!isGitUrl(key)) {
+      return gitDepErr;
+    }
+  }
+
+  return '';
+}
+
+
+/* SEMVER */
+
+function isObject(obj) {
+  return obj != null && !Array.isArray(obj) && typeof obj === 'object';
+}
+
+function isProjectName(str) {
+  const regex = /^[\w-]+\/[\w-]+$/;
+  return regex.test(str);
+}
+
+function toSemVer(str) {
+  const semver = /^[0-9]+.[0-9]+.[0-9]+$/;
+  if (!semver.test(str)) {
+    return null;
+  }
+
+  const parts = str.split('.');
+  return {
+    ctor: 'exact',
+    major: parseInt(parts[0]),
+    minor: parseInt(parts[1]),
+    patch: parseInt(parts[2])
+  };
+}
+
+function toSemVerRange(str) {
+  if (str == null) {
+    return null;
+  }
+
+  const result = str.split('<= v <');
+  if (result.length !== 2) {
+    return null;
+  }
+
+  const [lower, upper] = result;
+  lowerBound = toSemVer(lower.trim());
+  upperBound = toSemVer(upper.trim());
+
+  if (lowerBound == null || upperBound == null) {
+    return null;
+  }
+
+  return {
+    ctor: 'range',
+    lower: lowerBound,
+    upper: upperBound
+  };
 }
